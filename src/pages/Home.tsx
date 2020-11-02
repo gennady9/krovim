d09c0b24ff79a25e2e2656823acf8d1553fcdb8d
gennady9 <gennady9@gmail.com>
2020-11-02 17:45:56 +0200

    login page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,82 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonButton,
+  IonPage,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonIcon,
+} from "@ionic/react";
+import React, { useRef } from "react";
+import "./Home.css";
+import krovimLogo from "../resources/krovim-trans.png";
+import { logInOutline, personAddOutline } from "ionicons/icons";
 
 const Home: React.FC = () => {
+  const usernameInputRef = useRef<HTMLIonInputElement>(null);
+  const passwordInputRef = useRef<HTMLIonInputElement>(null);
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <div className="container">
+          <img src={krovimLogo} id="logo" alt="logo" />
+
+          <IonItem className="ion-margin-top" color="white">
+            <IonLabel
+              className="ion-text-right"
+              color="light"
+              position="floating"
+            >
+              שם משתמש
+            </IonLabel>
+            <IonInput
+              className="ion-text-right"
+              color="light"
+              type="text"
+              ref={usernameInputRef}
+            ></IonInput>
+          </IonItem>
+
+          <IonItem color="white">
+            <IonLabel
+              className="ion-text-right"
+              color="light"
+              position="floating"
+            >
+              סיסמא
+            </IonLabel>
+            <IonInput
+              className="ion-text-right"
+              color="light"
+              type="password"
+              ref={passwordInputRef}
+            ></IonInput>
+          </IonItem>
+
+          <IonRow className="ion-margin-top">
+            <IonCol>
+              <IonButton className="ion-text-center" color="light">
+                <IonIcon slot="start" icon={personAddOutline} />
+                הרשמה
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton className="ion-text-center" color="light">
+                <IonIcon slot="start" icon={logInOutline} />
+                התחברות
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </div>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
+
+//
